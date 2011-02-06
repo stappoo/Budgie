71393f9e0f2006ea72b0e00d04414c7e89752395
Steve Noyce <steve.noyce@stappoo.com>
2011-02-06 12:54:52 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Budgie::Application.config.secret_token = 'aca634592b4ef70c52d7463d288031d41f6fa57966bee0aa541036fae3f5dabc3dd77ac6801f18ed350eb92f0f812fd96a5dba509e3de23174c880013ea5104d'
